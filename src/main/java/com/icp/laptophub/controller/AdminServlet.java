a1879101c6b5aa4347ce0923081288ec5e0c92e8
Abhsiek Bagdas <abhisekbagdas@gmail.com>
2026-05-07 09:00:15 +0545

    Branch Destribution

--- src/main/java/com/icp/laptophub/controller/AdminServlet.java
+++ src/main/java/com/icp/laptophub/controller/AdminServlet.java
@@ -194,4 +194,5 @@ public class AdminServlet extends HttpServlet {
         public String getEmail() { return email; }
         public String getRole() { return role; }
     }
+//    Branches are distributed
 }
